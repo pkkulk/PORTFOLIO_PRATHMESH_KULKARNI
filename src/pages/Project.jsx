e09cdd775d7175b5ff0ee5ee2a82db1d69f5a2df
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-02-11 13:28:27 +0530

    ff

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -7,20 +7,55 @@ export default function Project(){
     <div className="border-2  w-screen  ">
     <h1 className="font-bold text-3xl text-teal-400 text-center" >Projects</h1>
       <div className="sm:grid-cols-1 grid p-7 lg:grid-cols-3 border-2 border-white rounded-md space-x-2 space-y-2 gap-4">
-           <div className="shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl ">
-            
-            
-            <h1 className="pl-16 pt-3 pb-2">Hospital Food Management </h1> <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/>
-            <hr/></div>
-           <div className="shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl "><h1 className="pl-16 pt-3 pb-2">Online MCQ examination </h1> <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/><hr/></div>
-           <div className="shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl "><h1 className="pl-16 pt-3 pb-2">Online MCQ examination </h1> <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/><hr/></div>
-           <div className="shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl "><h1 className="pl-16 pt-3 pb-2">Online MCQ examination </h1> <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/><hr/></div>
-           <div className="shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl "><h1 className="pl-16 pt-3 pb-2">Online MCQ examination </h1> <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/><hr/></div>
-           <div className="shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl "><h1 className="pl-16 pt-3 pb-2">Online MCQ examination </h1> <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/><hr/></div>
-           <div className="shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl "><h1 className="pl-16 pt-3 pb-2">Online MCQ examination </h1> <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/><hr/></div>
-           <div className="shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl "><h1 className="pl-16 pt-3 pb-2">Online MCQ examination </h1> <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/><hr/></div>
-          
-           </div>
+           <div className=" shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl ">
+            <h1 className="pl-16 pt-3 pb-2">Hospital Food Management </h1>
+             <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/>
+            <hr/>
+            <div className="w-fit  ">
+            <p className="mx-auto pl-11">A platform for buying books online.</p>
+             <a href="https://mern-stack-hospital-food-management-system-frontend.vercel.app" className="pl-28 text-blue-500 hover:underline">View Project</a>
+              </div>
+            </div>
+            <div className=" shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl ">
+            <h1 className="pl-16 pt-3 pb-2">Hospital Food Management </h1>
+             <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/>
+            <hr/>
+            <div className="w-fit  ">
+            <p className="mx-auto pl-11">A platform for buying books online.</p>
+             <a href="https://mern-stack-hospital-food-management-system-frontend.vercel.app" className="pl-28 text-blue-500 hover:underline">View Project</a>
+              </div>
+            </div>
+            <div className=" shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl ">
+            <h1 className="pl-16 pt-3 pb-2">Hospital Food Management </h1>
+             <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/>
+            <hr/>
+            <div className="w-fit  ">
+            <p className="mx-auto pl-11">A platform for buying books online.</p>
+             <a href="https://mern-stack-hospital-food-management-system-frontend.vercel.app" className="pl-28 text-blue-500 hover:underline">View Project</a>
+              </div>
+            </div>
+            <div className=" shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl ">
+            <h1 className="pl-16 pt-3 pb-2">Hospital Food Management </h1>
+             <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/>
+            <hr/>
+            <div className="w-fit  ">
+            <p className="mx-auto pl-11">A platform for buying books online.</p>
+             <a href="https://mern-stack-hospital-food-management-system-frontend.vercel.app" className="pl-28 text-blue-500 hover:underline">View Project</a>
+              </div>
+            </div>
+            <div className=" shadow-2xl hover:shadow-2xl hover:shadow-slate-700  border-2 ml-2 mt-2 h-72 rounded-xl ">
+            <h1 className="pl-16 pt-3 pb-2">Hospital Food Management </h1>
+             <hr/>  <img src={f} className="mx-auto h-44 w-full px-8 py-2"/>
+            <hr/>
+            <div className="w-fit  ">
+            <p className="mx-auto pl-11">A platform for buying books online.</p>
+             <a href="https://mern-stack-hospital-food-management-system-frontend.vercel.app" className="pl-28 text-blue-500 hover:underline">View Project</a>
+              </div>
+            </div>
+
+
+
+    </div>
     </div>
 
     
